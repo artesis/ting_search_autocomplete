c2d861ab922099aa5153e56eca1dcebd0d29daaf
Nicolae Grigoriu <ng@inlead.dk>
2019-03-27 13:12:09 +0200

    DBC-1415 - Implement open platform autocomplete functionality.

--- js/ting_search_autocomplete.js
+++ js/ting_search_autocomplete.js
@@ -4,32 +4,32 @@
       /**
        * Function enabling the advanced search feature.
        **/
-      function tingEnableAdvancedSearch() {
+      function tingEnableAdvancedSearch($search_form) {
 
         // Show advanced search.
         $('.fieldset-legend').show();
         $('.block-search-form form .extendsearch-advanced').addClass('enabled');
 
         // Enable autocomplete.
-        $('.block-search-form form input[name="search_block_form"]').autocomplete({
+        $search_form.autocomplete({
           minLength: 3,
           source: function(request, response) {
             jsonReq = $.getJSON(Drupal.settings.basePath + 'ting/autocomplete/' + request.term, {}, response);
           },
           search: function(event, ui) {
             // When a search is beginning, show the spinner.
-            $('.block-search-form form input[name="search_block_form"]').addClass('spinner');
-            $('.block-search-form form input[name="search_block_form"]').parent().addClass('spinner-wrapper');
+            $search_form.addClass('spinner');
+            $search_form.parent().addClass('spinner-wrapper');
           },
           open: function(event, ui) {
             // When a search is done, use this, to hide the spinner.
-            $('.block-search-form form input[name="search_block_form"]').removeClass('spinner');
-            $('.block-search-form form input[name="search_block_form"]').parent().removeClass('spinner-wrapper');
+            $search_form.removeClass('spinner');
+            $search_form.parent().removeClass('spinner-wrapper');
           },
           select: function(event, ui) {
             // Add the chosen value to the searchbox and submit.
             if (ui.item) {
-              $('.block-search-form form input[name="search_block_form"]').val(ui.item.value);
+              $search_form.val(ui.item.value);
               $('#search-block-form').submit();
             }
           }
@@ -39,16 +39,16 @@
       /**
        * Function disabling the advanced search feature.
        **/
-      function tingDisableAdvancedSearch() {
+      function tingDisableAdvancedSearch($search_form) {
         // Hide advanced search.
         $('.block-search-form form .extendsearch-advanced').removeClass('enabled');
-        if ($('.block-search-form form input[name="search_block_form"]').hasClass('spinner')) {
-          $('.block-search-form form input[name="search_block_form"]').removeClass('spinner');
-          $('.block-search-form form input[name="search_block_form"]').parent().removeClass('spinner-wrapper');
+        if ($search_form.hasClass('spinner')) {
+          $search_form.removeClass('spinner');
+          $search_form.parent().removeClass('spinner-wrapper');
         }
 
         // Disable autocomplete.
-        $('.block-search-form form input[name="search_block_form"]').autocomplete({
+        $search_form.autocomplete({
 
           // Overwrite source function.
           source: [],
@@ -70,8 +70,8 @@
       /**
        * Function moves advanced search values to default search field.
        **/
-      function tingMoveAdvancedSearchValues() {
-        var fieldValue = $('.block-search-form form input[name="search_block_form"]').val();
+      function tingMoveAdvancedSearchValues($search_form) {
+        var fieldValue = $search_form.val();
         $('.block-search-form .extendsearch-advanced input').each(function() {
           if ($(this).val().length > 0) {
             fieldValue += ' ' + $(this).val();
@@ -81,26 +81,26 @@
         $('.block-search-form form .ui-autocomplete-input').val(fieldValue);
       }
 
+      let $search_form = $('.block-search-form form input[name="search_block_form"]', context);
       // Check if default selected search provider is ting.
-      if ($('input[name="search_provider"]:checked').val() == 'ting') {
-        tingEnableAdvancedSearch();
+      if ($('input[name="search_provider"]:checked').val() === 'ting') {
+        tingEnableAdvancedSearch($search_form);
       }
       else {
-        tingDisableAdvancedSearch();
-        tingMoveAdvancedSearchValues();
+        tingDisableAdvancedSearch($search_form);
+        tingMoveAdvancedSearchValues($search_form);
       }
 
       // Register event for clicking search provider radio.
-      $('input#edit-search-provider-ting.form-radio').live('click', function() {
-        tingEnableAdvancedSearch();
+      $('input#edit-search-provider-ting.form-radio').on('click', function() {
+        tingEnableAdvancedSearch($search_form);
       });
 
       // Register event for clicking WEBSITE and E-RESOURCES.
-      $('input#edit-search-provider-node.form-radio, input#edit-search-provider-meta.form-radio').live('click', function() {
-        tingDisableAdvancedSearch();
-        tingMoveAdvancedSearchValues();
+      $('input#edit-search-provider-node.form-radio, input#edit-search-provider-meta.form-radio').on('click', function() {
+        tingDisableAdvancedSearch($search_form);
+        tingMoveAdvancedSearchValues($search_form);
       });
-
     }
   };
 } (jQuery));
